be3ab94bbb9cd8b801c2447c2fb99a891b611a44
Manthan <pmanthan549@gmail.com>
2025-05-15 11:59:00 +0530

    added dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,14 @@ import 'package:bloom/screens/auth/settings.dart';
 import 'package:bloom/screens/auth/setup.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
